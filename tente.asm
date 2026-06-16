6697dfd6d82d918dd77fcdb750d6ab217018f43c
Felipe Tagawa <felipereisbkp@gmail.com>
2026-06-16 18:10:14 -0300

    feat: add assembly files

---
+++ tente.asm
@@ -0,0 +1,23 @@
+.data
+len: .byte 9
+data: .half 10, 4, 15, 10, 20, 24, 30, 28, 25
+
+
+.text
+start:
+    addiu $8, $0, 0x0000    # read = 0
+    addiu $9, $0, 0x0011    # write = 0x11 = começo do destino + 8
+    addiu $10, $0, 9        # contador = 9
+    addiu $12, $0, 1        # constante 1
+
+loop:
+    beq $10, $0, fim
+    lw $11, 0($8)
+    sw $11, 0($9)
+    addu $8, $8, $12
+    subu $9, $9, $12
+    subu $10, $10, $12
+    j loop
+
+fim:
+    j fim
